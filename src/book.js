936e4298d4dc3eff968915a028ef341358ce1d29
Rachel Prather <rachelprather1@gmail.com>
2023-03-23 15:26:34 -0700

    Complete iteration 1

--- src/book.js
+++ src/book.js
@@ -1,12 +1,50 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`;
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  return character = {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  }
+}
+
+function saveReview(newReview, reviews) {
+  if (reviews.includes(newReview) === false) {
+    return reviews.push(newReview);
+  }
+}
+
+function calculatePageCount(title) {
+  titleWords = title.split('');
+  titleLetters = [];
+  for (var i = 0; i < titleWords.length; i++) {
+      titleLetters.push(titleWords[i]);
+  }
+  numLetters = titleLetters.length;
+  pageCount = numLetters * 20;
+  return pageCount;
+}
+
+function writeBook(title, character, genre) {
+  return book = {
+    title: title,
+    mainCharacter: character,
+    pageCount: calculatePageCount(title),
+    genre: genre
+  }
+}
+
+function editBook(book) {
+  return book.pageCount *= 0.75
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
