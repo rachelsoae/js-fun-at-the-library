e4e5efe0e1f1da999147288ad2ad533785aee187
Rachel Prather <rachelprather1@gmail.com>
2023-03-24 18:54:00 -0700

    Complete refactor extension

--- src/library.js
+++ src/library.js
@@ -25,20 +25,45 @@ function checkoutBook(library, bookTitle, genre) {
   return `Sorry, there are currently no copies of ${bookTitle} available at the ${library.name}.`
 }
 
+// function takeStock(library, genre) {
+//     if (genre === undefined) {
+//       var numFantasy = library.shelves['fantasy'].length;
+//       var numFiction = library.shelves['fiction'].length;
+//       var numNonFiction = library.shelves['nonFiction'].length;
+
+//       var totalBooks = numFantasy + numFiction + numNonFiction;
+//       return `There are a total of ${totalBooks} books at the ${library.name}.`
+//     } else {
+//       var numBooksOnShelf = library.shelves[genre].length
+
+//       return `There are a total of ${numBooksOnShelf} ${genre} books at the ${library.name}.`
+//     }
+// }
+
 function takeStock(library, genre) {
   if (genre === undefined) {
-    var numFantasy = library.shelves['fantasy'].length;
-    var numFiction = library.shelves['fiction'].length;
-    var numNonFiction = library.shelves['nonFiction'].length;
+  var numFantasy = library.shelves['fantasy'].length;
+  var numFiction = library.shelves['fiction'].length;
+  var numNonFiction = library.shelves['nonFiction'].length;
 
-    var totalBooks = numFantasy + numFiction + numNonFiction;
-    return `There are a total of ${totalBooks} books at the ${library.name}.`
+  var totalBooks = numFantasy + numFiction + numNonFiction;
+  return `There are a total of ${totalBooks} books at the ${library.name}.`;
   } else {
-    var numBooksOnShelf = library.shelves[genre].length
+    var shelf = library.shelves[genre];
 
-    return `There are a total of ${numBooksOnShelf} ${genre} books at the ${library.name}.`
+    for (var i = 0; i < shelf.length; i++) {
+      if (genre === shelf[i].genre) {
+        var numBooksOnShelf = shelf.length;
+    
+        return `There are a total of ${numBooksOnShelf} ${genre} books at the ${library.name}.`;
+
+      } else if (genre !== shelf) {
+        return `This book is not on the correct shelf.`;
+      }
+    }
   }
 }
+ 
 
 //shelf = library.shelves[shelf]
 //in loop, book = library.shelves[shelf][i]
